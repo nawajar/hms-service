074aa8e4ee28e7a54d89d78d49d2756a051aaf3f
nawajar <nawapb@gmail.com>
2024-08-05 09:49:22 +0700

    translate

--- hms/src/i18n/translate.ts
+++ hms/src/i18n/translate.ts
@@ -1,12 +1,19 @@
 const messages = {
     laos: {
+        menu: {
+            dashboard: 'ຖາພຮວມ',
+            booking: 'ກາຮຂອງຫ້ອງພິກ',
+            room_status: 'ສຖານະຫ້ອງ',
+            room_schedule: 'ຕາຮາງກາຮຂອງ',
+            booking_history: 'ກາຮຈອງທີ່ຜ່ານມາ',
+        },
         dashboard: {
-         book_today: 'ຮາຍກາຮຂອງວິນນີ້',
-         summary_amt: 'ຍອດຮວມ',
-         paid: 'ຂ່າຍແລ້ວ',
-         un_paid: 'ຍິງໄມ່ຂ່າຍແລ້ວ',
-         available_room: 'ຫ້ອງວ່າງ',
-         need_clean_room: 'ຫ້ອງຮອທຳຕວາມສະອາດ'
+            book_today: 'ຮາຍກາຮຂອງວິນນີ້',
+            summary_amt: 'ຍອດຮວມ',
+            paid: 'ຂ່າຍແລ້ວ',
+            un_paid: 'ຍິງໄມ່ຂ່າຍ',
+            available_room: 'ຫ້ອງວ່າງ',
+            need_clean_room: 'ຫ້ອງຮອທຳຕວາມສະອາດ'
         }
     }
   }
